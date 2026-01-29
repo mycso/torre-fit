a96f35e377c4883ed3aacd9d95afec4ed2389ed9
mycso <mycso@live.co.uk>
2026-01-29 22:38:52 +0000

    tweak: adjust layout and header css with tailwind

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,23 +16,27 @@ export default function RootLayout({
       <body
         className="min-h-screen bg-zinc-50 text-zinc-900"
       >
-        <div className="mx-auto max-w-5xl px-4 py-8">
-          <header className="mb-8 p-3 flex items-center justify-between">
+        <header className="p-3 flex items-center justify-between bg-gray-700 text-lime-300">
+          <div className="flex w-7xl items-center justify-between mx-auto p-2 lg:px-8">
             <div>
               <h1 className="text-2xl font-semibold tracking-tight">Torre Fit</h1>
-              <p className="text-sm text-zinc-600">
+              <p className="text-sm text-lime-500">
                 Load a profile, search jobs, and see a quick skills match.
               </p>
             </div>
+            <div>
             <a
               href="https://torre.ai"
               target="_blank"
               rel="noreferrer"
-              className="text-sm text-zinc-600 hover:text-zinc-900"
+              className="text-sm text-lime-400 hover:text-lime-500"
             >
               torre.ai
             </a>
-          </header>
+            </div>
+          </div>
+        </header>
+        <div className="mx-auto max-w-5xl px-4 py-8">
           {children}
           <footer className="mt-10 border-t pt-6 text-xs text-zinc-500">
             Built for capability assesment - focus on architecture & clarity.
